840ccd06d822aa74590a65ebd64db7d3758bc71f
Pugalarasan <75373225+litt1eb0yy@users.noreply.github.com>
2022-05-13 20:56:20 +0530

    Update fav.sh

--- fav.sh
+++ fav.sh
@@ -4,5 +4,3 @@ read -p "Enter Favicon URL: " fav
 hash=$(curl -s -L -k $fav | python3 -c 'import mmh3,sys,codecs; print(mmh3.hash(codecs.encode(sys.stdin.buffer.read(),"base64")))')
 ###Found
 echo Generated Hash:$hash
-shodan search http.favicon.hash:$hash
-shodan search http.favicon.hash:$hash --fields ip_str,port --separator " " | awk '{print $1 ":" $2}'
